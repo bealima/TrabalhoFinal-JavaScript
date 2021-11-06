020a96440579175df622d9c9c4c55abecb771d7c
Ana <bialima.150324@gmail.com~>
2021-11-06 10:42:14 -0300

    criação arquivo json

---
+++ HomeRecrutador/telaHomeRecrutador.js
@@ -0,0 +1,17 @@
+/*Acessar Detalhamento de vagas :
+ event.listener("click", irpara página de detalhamento da vaga)*/
+
+//botão sair: Logout- voltar para página de login , limpar dados 
+
+/* Cadatrar vaga: 
+
+    tela de cadastro : 
+
+    - título
+    - descrição
+    - remuneração
+    - botão voltar
+    - botão cadastar
+
+*/
+
---
+++ HomeTrabalhador/telaHomeTrabalhador.js
@@ -0,0 +1,8 @@
+/* Listra vagas e Acessar Detalhamento de vagas :
+ event.listener("click", irpara página de detalhamento da vaga)*/
+
+
+//botão sair: Logout- voltar para página de login , limpar dados 
+axios.post('http://localhost:3000/usuarios', {nome: "Ana", idade: 27});
+
+console.log("olaá")
