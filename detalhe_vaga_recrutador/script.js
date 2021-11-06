11f99aef0fa2e260ccb7938b450d7fff134f0899
Deiverson Cruz <dcruzbhha2@gmail.com>
2021-11-06 11:12:09 -0300

    Adicionando paginas de detalhe da vaga para recrutador e para trabalhador

---
+++ detalhe_vaga_recrutador/script.js
@@ -0,0 +1,17 @@
+let trabalhador = {
+    nome = '',
+    dataNascimento = '',
+    email = '',
+    senha = '',
+    primeiroEmprego = null,
+    tipoUsuario = '',
+    vagasCadastradas = [{id:0, rejeitado:false}]
+}
+
+let vaga = {
+    titulo = '',
+    descricao = '',
+    remuneracao = '',
+    candidatos = []
+}
+
