541af955ee579440cdff4ed79a1e0e5f538fcf44
Deiverson Cruz <dcruzbhha2@gmail.com>
2021-11-07 16:02:45 -0300

    Funcionalidade get na pagina de vagas

---
+++ detalhe_vaga_trabalhador/script.js
@@ -0,0 +1,37 @@
+const vaga = () =>{
+    // let elementoOrigem = document.getElementById('home');
+    // let elementoDestino = document.getElementById('edit');
+    // elementoOrigem.className = elementoOrigem.className.replace('d-flex', 'd-none');
+    // elementoDestino.className = elementoDestino.className.replace('d-none', 'd-flex');
+
+    let id = 13;
+    let tituloVaga =  document.getElementById('titulo');
+    let descricaoVaga = document.getElementById('descricao');
+    let remuneracao = document.getElementById('remuneracao');
+    let nomeCandidato = document.getElementById('nome');
+    let dataNascimentoCandidato = document.getElementById('birthday');
+
+    axios.get(`http://localhost:3000/vagas/${id}`)
+    .then((response) => {
+    let responseData = response.data;
+    tituloVaga.innerText = responseData.titulo;
+    descricaoVaga.innerText = responseData.descricao;
+    remuneracao.innerText = responseData.remuneracao;
+
+/* --------------- Candidato ---------------- */
+
+    let arrayCandidatos = responseData.candidatos;
+    let spreadCandidatos = [...arrayCandidatos];
+    let candidato = spreadCandidatos.find( user => user.email === 'dcruzbhha2@gmail.com');
+    console.log(candidato)
+    
+    
+
+    nomeCandidato.innerText = candidato.nome;
+    dataNascimentoCandidato.innerText = candidato.dataNascimento;
+    // console.log(candidato);
+    })
+}
+
+vaga();
+
