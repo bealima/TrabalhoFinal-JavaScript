15d09f7a8581d8479deb6f1c73987d6306c905ca
Deiverson Cruz <dcruzbhha2@gmail.com>
2021-11-07 00:45:10 -0300

    Ajustes e testes no Axios.get para login

--- tela-login/login.js
+++ tela-login/login.js
@@ -1,12 +1,14 @@
 const validarLogin = () => {
-    axios.get('http://localhost:3000/candidatos', candidato)
+    axios.get('http://localhost:3000/usuarios' )
         .then(response => {
             let emailDigitado = document.getElementById('email-input-login').value;
             let senhaDigitada = document.getElementById('password-input-login').value;
             
             let podeLogar = response.data.some(c => c.email === emailDigitado && c.senha === senhaDigitada);
-
+            
+            
             if(podeLogar) {
+                console.log('It works')
                 irPara('login', 'home');
             }
         })
