fe3b26287ad037e5b6a8f4c11eb93fed0d49e85a
Ana <bialima.150324@gmail.com~>
2021-11-07 23:17:36 -0300

    função listar vagas cadastradas home trabalhador funcionando

--- HomeRecrutador/telaCadastrarVaga.js
+++ HomeRecrutador/telaCadastrarVaga.js
@@ -1,56 +1,3 @@
-const listarVagas = () =>{
-
-    axios.get('http:localhost:3000/vagas')
-    .then((response) => {
-        let responseData = response.data;
-        
-        let blocoVagas = document.getElementById('block-Vagas');
-        console.log(response);
-
-        responseData.forEach(e => {
-            let vagasHome = document.createElement('div');
-            vagasHome.setAttribute('class', 'classVagasHome');
-            blocoVagas.appendChild(vagasHome);
-
-            let spanTitulo = document.createElement('span');
-            spanTitulo.setAttribute('class', 'classSpan');
-            vagasHome.appendChild(spanTitulo);
-
-            let h4Titulo = document.createElement('h4');
-            h4Titulo.setAttribute('class', 'classh4');
-            h4Titulo.innerText = 'Titulo: '
-            spanTitulo.appendChild(h4Titulo);
-
-            let divTitulo = document.createElement('div');
-            divTitulo.setAttribute('class', 'classDivTitulo')
-            spanTitulo.appendChild(divTitulo);
-
-            let spanRemuneracao = document.createElement('span');
-            spanRemuneracao.setAttribute('class', 'classSpan');
-            vagasHome.appendChild(spanRemuneracao);
-
-            let h4Remuneracao = document.createElement('h4');
-            h4Remuneracao.setAttribute('class', 'classh4');
-            h4Remuneracao.innerText = 'Remuneração: R$ '
-            spanRemuneracao.appendChild(h4Remuneracao);
-
-            let divRemuneracao = document.createElement('div');
-            divRemuneracao.setAttribute('class', 'classDivRemuneracao')
-            spanRemuneracao.appendChild(divRemuneracao);
-
-
-            divTitulo.innerText = e.titulo 
-            divRemuneracao.innerText = e.remuneracao
-            //console.log(element);
-        });
-            
-    });
-    
-}
-
-listarVagas()
-
-
 
 const abrirTelacadastroVaga = () => {
     let btnCadastrarVaga =  document.getElementById('btnCadastrarVagaHome');
@@ -58,10 +5,9 @@ const abrirTelacadastroVaga = () => {
 };
 
 const telaCadastroVaga = () => {
----------- Apagando Main --------------------------------------
-
-    let blockVagas = document.getElementById('block-Vagas');
-    blockVagas.setAttribute('style', 'display:none');
+//---------- Apagando Main --------------------------------------
+    let blocoMain = document.getElementById('blocoMain');
+    blocoMain.setAttribute('style', 'display:none');
 
     
     let main = document.getElementById('main');
@@ -155,3 +101,4 @@ const telaCadastroVaga = () => {
 }
 
 abrirTelacadastroVaga()
+
--- HomeTrabalhador/telaHomeTrabalhador.js
+++ HomeTrabalhador/telaHomeTrabalhador.js
@@ -1,25 +1,53 @@
-/* Listra vagas e Acessar Detalhamento de vagas :
- event.listener("click", irpara página de detalhamento da vaga)*/
 
 
-//botão sair: Logout- voltar para página de login , limpar dados 
-//axios.post('http://localhost:3000/usuarios', {nome: "Ana", idade: 27});
-//console.log("olaá")
+const listarVagas = () =>{
 
-let usuario = {
-    nome = '',
-    dataNascimento = '',
-    email = '',
-    senha = '',
-    primeiroEmprego = null,
-    tipoUsuario = '',
-    vagasCadastradas = [{id:0, rejeitado:false}]
+    axios.get('http:localhost:3000/vagas')
+    .then((response) => {
+        let responseData = response.data;
+        
+        let blocoVagas = document.getElementById('block-Vagas');
+        console.log(response);
+
+        responseData.forEach(e => {
+            let vagasHome = document.createElement('div');
+            vagasHome.setAttribute('class', 'classVagasHome');
+            blocoVagas.appendChild(vagasHome);
+
+            let spanTitulo = document.createElement('span');
+            spanTitulo.setAttribute('class', 'classSpan');
+            vagasHome.appendChild(spanTitulo);
+
+            let h4Titulo = document.createElement('h4');
+            h4Titulo.setAttribute('class', 'classh4');
+            h4Titulo.innerText = 'Titulo: '
+            spanTitulo.appendChild(h4Titulo);
+
+            let divTitulo = document.createElement('div');
+            divTitulo.setAttribute('class', 'classDivTitulo')
+            spanTitulo.appendChild(divTitulo);
+
+            let spanRemuneracao = document.createElement('span');
+            spanRemuneracao.setAttribute('class', 'classSpan');
+            vagasHome.appendChild(spanRemuneracao);
+
+            let h4Remuneracao = document.createElement('h4');
+            h4Remuneracao.setAttribute('class', 'classh4');
+            h4Remuneracao.innerText = 'Remuneração: R$ '
+            spanRemuneracao.appendChild(h4Remuneracao);
+
+            let divRemuneracao = document.createElement('div');
+            divRemuneracao.setAttribute('class', 'classDivRemuneracao')
+            spanRemuneracao.appendChild(divRemuneracao);
+
+
+            divTitulo.innerText = e.titulo 
+            divRemuneracao.innerText = e.remuneracao
+            //console.log(element);
+        });
+            
+    });
+    
 }
 
-let vaga = {
-    titulo = '',
-    descricao = '',
-    remuneracao = '',
-    candidatos = [],
-    rejeitado = false
-}
+listarVagas()
